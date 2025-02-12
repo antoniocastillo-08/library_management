8b670e120e909e6fffc02b295941191d2c0e9e4a
Antonio <antoniocj04@gmail.com>
2025-02-12 23:07:16 +0100

    Vistas ajustadas

--- __manifest__.py
+++ __manifest__.py
@@ -1,21 +1,19 @@
+# -*- coding: utf-8 -*-
 {
     'name': 'Library Management',
     'version': '1.0',
     'category': 'Library',
     'summary': 'Module for managing library books, authors, categories, and loans',
-    'author': 'Your Name',
-    'website': 'https://www.yourwebsite.com',
+    'author': 'Antonio',
     'depends': ['base'],
     'data': [
-        #'views/books_view.xml',
         'security/ir.model.access.csv',
-        'views/menu.xml',
         'views/author_view.xml',
+        'views/books_view.xml',
         'views/users_view.xml',
-
-       #' 'views/category_view.xml',
-        #''views/loan_view.xml',
-       
+        'views/loan_view.xml',
+        'views/category_view.xml',
+        'views/menu.xml',
     ],
     'installable': True,
     'application': True,
